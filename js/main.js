a964883176d8ab54952f27cf5cad476867e2cf53
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-02-13 23:54:38 +0300

    nubos

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(function(){
+	$('.btn-menu').click(function(){	
+		$(this).toggleClass('active');
+		$('.header__menu .menu').toggleClass('active');		
+	});
+
+$('.menu .menu-link').click(function(){	
+		$('.header__menu .menu').removeClass('active');	
+		$('.btn-menu').removeClass('active');		
+	});
+
+$('.slider').slick({
+prevArrow: $('.prev'),
+nextArrow: $('.next'),
+slidesToShow: 2,
+responsive: [
+{
+    breakpoint: 992,
+    settings: {
+    slidesToShow: 1,
+   
+    }
+  },
+]
+});
+
+$('.logo-slider').slick({
+prevArrow: false,
+nextArrow: false, 
+slidesToShow: 4,
+dots: true,
+responsive: [
+{
+    breakpoint: 992,
+    settings: {
+    slidesToShow: 2,
+    centerMode: true,
+   
+    }
+  },
+  {
+    breakpoint: 675,
+    settings: {
+    slidesToShow: 1,
+    centerMode: true,
+   
+    }
+  },
+]
+});
+
+
+
+});
+
+// -------start mobil dropdown menu--------
+if(window.innerWidth < 992) {
+ $('.header__dropdown-link').click(function(){	
+		$(this).toggleClass('active');		
+	});
+}
+// -------end mobil dropdown menu--------
+
